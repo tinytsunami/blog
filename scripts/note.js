50567c94a42d70e19453a9681517131764e567d3
tinytsunami <z27619273@gmail.com>
2026-03-21 17:43:38 +0800

    feat: add note block, refer to next theme

--- scripts/note.js
+++ scripts/note.js
@@ -1,11 +1,12 @@
 'use strict';
 
 function note(args) {
-
+    const type = args[0] || 'info';
+    return `<div class="note ${type}">`;
 }
 
 function endnote(args) {
-
+    return '</div>';
 }
 
 hexo.extend.tag.register('note', note);
